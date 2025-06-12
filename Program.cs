045be86ff90dac36583b4d6e8aea8bc4f42b8330
SeyedMohammadAminAlemi <aminalemi02@gmail.com>
2025-06-12 16:51:47 +0330

    main program , bug fixed !

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketSystem.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+
+// این خط حیاتی برای DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Use middleware
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+builder.Services.AddSession();
+
+app.UseSession();
+
+// Map routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
